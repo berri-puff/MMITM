02a7b25a0ac45209cdbe1473684953105dc749af
Chris Maylor <chrismaylor1@hotmail.com>
2024-01-02 14:30:28 +0000

    tiny bit of clearing up

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,18 +1,7 @@
-
-import InvitesList from "./Invitations/InvitationsList";
-import Login from "./LogIn";
-
 export const Home = () => {
   return (
     <>
      <h2>Home</h2>
-    
- 
-   
-     <p>
-      user login page 
-      <Login/>
-     </p>
     </>
   )
 
