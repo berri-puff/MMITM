85582560805ea97d6db07ce582350f0df329564a
addleton <michaeladdleton1@gmail.com>
2024-01-02 16:52:49 +0000

    adds axios and queries google places api

--- src/components/Suggestions.tsx
+++ src/components/Suggestions.tsx
@@ -1,9 +1,28 @@
-import { SuggestionsList } from "./Suggestions/SuggestionsList"
-import { SuggestionsMap } from "./Suggestions/SuggestionsMap"
+import axios from "axios";
+import { SuggestionsList } from "./Suggestions/SuggestionsList";
+import { SuggestionsMap } from "./Suggestions/SuggestionsMap";
+import { useEffect, useState } from "react";
 
 export const Suggestions = () => {
-    return <div>
-        <SuggestionsList/>
-        <SuggestionsMap/>
+  const [places, setPlaces] = useState([]);
+  const coordinates = "53.7767590187112, -2.2348233608134205";
+  const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
+
+  const getPlaces = async () => {
+    const { data } = await axios.get(
+      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${coordinates}&radius=50000&key=${apiKey}`
+    );
+    setPlaces(data.results);
+  };
+
+  useEffect(() => {
+    getPlaces();
+  }, []);
+
+  return (
+    <div>
+      <SuggestionsList />
+      <SuggestionsMap />
     </div>
-}
+  );
+};
