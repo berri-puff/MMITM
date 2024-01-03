8ea5c2d00491d17cc5e2b0a77a12296ad7aa927c
aislingkelly <aisling.kelly45@gmail.com>
2024-01-03 17:34:31 +0000

    fix tailwind

--- src/utils/api-ak.ts
+++ src/utils/api-ak.ts
@@ -4,10 +4,24 @@ const api = axios.create({
   baseURL: 'https://maps.googleapis.com/maps/api/distancematrix',
 });
 
-export const getDistance = () => {
-  const origins =
-    '51.5136375825016, -0.1365400240806151|51.46222995063997, 0.10913959392165778';
-  const destination = '51.51104942119124, -0.03696107287824183';
+// export const getDistance = () => {
+//   const origins =
+//     '51.5136375825016, -0.1365400240806151|51.46222995063997, 0.10913959392165778';
+//   const destination = '51.51104942119124, -0.03696107287824183';
+//   const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
+//   const mode = 'walking';
+
+//   return api
+//     .get(
+//       `/json?destinations=${destination}&origins=${origins}&units=metric&mode=${mode}&key=${apiKey}`
+//     )
+//     .then((response) => {
+//       console.log(response, 'res');
+//       return response.data;
+//     });
+// };
+
+export const getDistance = (origins: string, destination: string) => {
   const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
   const mode = 'walking';
 
@@ -16,7 +30,7 @@ export const getDistance = () => {
       `/json?destinations=${destination}&origins=${origins}&units=metric&mode=${mode}&key=${apiKey}`
     )
     .then((response) => {
-      console.log(response, 'res');
+      console.log(response.data, 'res');
       return response.data;
     });
 };
