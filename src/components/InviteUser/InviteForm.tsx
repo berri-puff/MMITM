3c0fbfc271ade0598b3258bd0f1186826460705a
berri-puff <jennyzhang0207@gmail.com>
2024-01-04 16:04:07 +0000

    adds functionality to update documents, error ongoing with updating itineraries collection - reason unknown

--- src/components/InviteUser/InviteForm.tsx
+++ src/components/InviteUser/InviteForm.tsx
@@ -2,7 +2,7 @@ import { useContext, useState } from "react";
 import { UserContext } from "../../contexts/UserContext";
 import { Link } from "react-router-dom";
 import db from "../../lib/fireBaseConfig";
-import { collection, getDocs } from "@firebase/firestore";
+import { collection, getDocs, doc, updateDoc } from "@firebase/firestore";
 import { Users } from "../../types";
 
 export const InviteForm: React.FC = () => {
@@ -10,16 +10,18 @@ export const InviteForm: React.FC = () => {
   const [foundUser, setFoundUser] = useState<Users[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const { user } = useContext(UserContext);
-
   const retrieveUsers = async (searchUser: string) => {
     try {
       const querySnapshot = await getDocs(collection(db, "users"));
       const data: Users[] = querySnapshot.docs.map((person) => {
-        return { ...person.data() };
+        return {...person.data() };
       });
       setFoundUser(
         data.filter((person) => {
-          if (person.first_name === searchUser || person.username === searchUser) {
+          if (
+            person.first_name === searchUser ||
+            person.username === searchUser
+          ) {
             return { ...person };
           }
         })
@@ -41,6 +43,21 @@ export const InviteForm: React.FC = () => {
     retrieveUsers(searchInput);
   }
 
+  const sendIntinerary = async(invitee : Users[]) : Promise<void> => {
+    console.log(invitee.username, 'in the function')
+    const patchMessage = {
+        attendees : {
+            invitee_1 : {
+            transportation : invitee.username
+            }
+        }
+       
+    }
+    const docRef = doc(db, 'itineraries', 'uvT1e8fJHxSQb3x3LWOF')
+updateDoc(docRef, patchMessage)
+  
+  }
+
   if (user === "Nobody") {
     return (
       <p>
@@ -49,23 +66,6 @@ export const InviteForm: React.FC = () => {
     );
   }
   if (isLoading) {
-  return (  <section>
-      <form onSubmit={searchForUser}>
-        <label htmlFor="invite-user">
-          Search by firstname:
-          <input
-            id="searchUserInput"
-            type="text"
-            placeholder="second"
-            onChange={handleSearchUser}
-            value={searchInput}
-          />
-        </label>
-        <button>Search</button>
-      </form>
-      <p>Loading!</p>
-    </section>)
-  } else {
     return (
       <section>
         <form onSubmit={searchForUser}>
@@ -81,20 +81,39 @@ export const InviteForm: React.FC = () => {
           </label>
           <button>Search</button>
         </form>
+        <p>Loading!</p>
+      </section>
+    );
+  } else {
+    return (
+      <section>
+        <form onSubmit={searchForUser}>
+          <label htmlFor="invite-user">
+            Search by firstname or username:
+            <input
+              id="searchUserInput"
+              type="text"
+              placeholder="second"
+              onChange={handleSearchUser}
+              value={searchInput}
+            />
+          </label>
+          <button>Search</button>
+        </form>
         {foundUser.length !== 0 ? (
-          <ul>
+          <>
             {foundUser.map((person) => {
               return (
-                <li key={person.id}>
+                <ul key={person.id}>
                   <p>Name: {person.first_name}</p>
                   <p>Username: {person.username}</p>
-                  <button>Invite!</button>
-                </li>
+                  <button onClick={()=>{sendIntinerary(person)}}>Invite!</button>
+                </ul>
               );
             })}
-          </ul>
+          </>
         ) : (
-          <p>No one here yet</p>
+          <p>Start inviting friends!</p>
         )}
       </section>
     );
