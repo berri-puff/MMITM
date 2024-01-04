9bfea9a8c779f7775de59ee2738734150a30efa4
addleton <michaeladdleton1@gmail.com>
2024-01-04 14:57:15 +0000

    integrates crosshair in places api call to retrieve 100 places, refactored utils function to get unique places and return top 25

--- src/components/Suggestions.tsx
+++ src/components/Suggestions.tsx
@@ -1,38 +1,51 @@
 import { SuggestionsList } from "./Suggestions/SuggestionsList";
 import { SuggestionsMap } from "./Suggestions/SuggestionsMap";
 import { useEffect, useState } from "react";
-import { sortPlaces } from "../utils/utils";
-import { Place } from "../types";
+import { convertCoordsToCrosshair, sortPlaces } from "../utils/utils";
+import { Place, SuggestionsProps } from "../types";
 import { getAllPlaces, getPlaces } from "../utils/api-ma";
 
 export const Suggestions = (props: SuggestionsProps) => {
+  const crosshair = convertCoordsToCrosshair(props);
   const [places, setPlaces] = useState<Place[]>([]);
   const [isSorted, setIsSorted] = useState(false);
-  const coordinatesArr = ['53.31021296380358, -1.267900019270819', '53.17276291450375, -1.261083628609555', '53.25840402161476, -1.4630509181253877', '53.252622852967264, -0.9992025977062506', '53.24587716867708, -1.2485210699315368']
-  const coordinates = "53.7767, -2.2348";
-  const lat: string = "53.7767";
-  const lng: string = "-2.2348";
   const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    getAllPlaces(coordinatesArr, setPlaces, apiKey);
+    const fetchData = async () => {
+      try {
+        const placesData = await getAllPlaces(crosshair, setPlaces, apiKey);
+        const placesArr = placesData.map((place) => {
+          return place.results.map((result) => {
+            return result;
+          });
+        });
+        setPlaces(placesArr.flat());
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
   }, []);
 
   useEffect(() => {
-    if (places.length === 60) {
+    if (places.length === 100) {
       setPlaces((currPlaces) => {
         const sortedPlaces = sortPlaces(currPlaces);
         return sortedPlaces;
       });
       setIsSorted(true);
+      setLoading(false);
     }
   }, [places]);
 
-  if (isSorted) {
+  if (loading) {
+    return <h3>Loading....</h3>;
+  } else if (isSorted && !loading) {
     return (
       <div>
         <SuggestionsList places={places} />
-        <SuggestionsMap lat={lat} lng={lng} places={places} />
       </div>
     );
   }
--- src/utils/api-ma.ts
+++ src/utils/api-ma.ts
@@ -1,40 +1,24 @@
 import axios from "axios";
 import { Place } from "../types";
+import { convertCrosshairToArray } from "./utils";
 
-export const getPlaces = async (coordinatesArr, setPlaces, apiKey) => {
-  const delay = (ms: number) =>
-    new Promise((resolve) => setTimeout(resolve, ms));
-  let resultsCount = 0;
-  let nextPageToken = null;
-  try {
-    while (resultsCount < 60) {
-      let url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${coordinates}&type=cafe&opennow&rankby=distance&key=${apiKey}`;
-      if (nextPageToken) {
-        url += `&pagetoken=${nextPageToken}`;
-      }
-      let { results, next_page_token: newNextPageToken } = await getMorePlaces(
-        url
-      );
-
-      setPlaces((currPlaces: Place[]) => [...currPlaces, ...results]);
-      resultsCount += results.length;
-      nextPageToken = newNextPageToken;
-      await delay(1000);
-    }
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-export const getMorePlaces = async (url: string) => {
+export const getPlaces = async (coordinate, setPlaces, apiKey) => {
+  let url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${coordinate}&type=cafe&opennow&rankby=distance&key=${apiKey}`;
   const { data } = await axios.get(url);
-  return data;
+  return data
 };
 
-export const getAllPlaces = async (coordinatesArr, setPlaces, apiKey) => {
-  const placesPromises = [] 
-  await coordinatesArr.map((coordinates) => {
-    return getPlaces(coordinates, setPlaces, apiKey);
-  });
-  console.log(placesPromises)
+export const getAllPlaces = async (crosshair, setPlaces, apiKey) => {
+  const coordinates = convertCrosshairToArray(crosshair);
+  const placesPromises = [];
+  await coordinates.map((coordinate) => {
+    placesPromises.push(getPlaces(coordinate, setPlaces, apiKey));
+  })
+  try {
+    const dataArr = await Promise.all(placesPromises)
+    return dataArr
+  } catch (err) {
+    console.log(err)
+  }
+
 };
