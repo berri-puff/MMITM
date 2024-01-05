153542076ad18ca2da7f882582c8cae989417520
Chris Maylor <chrismaylor1@hotmail.com>
2024-01-05 11:46:26 +0000

    adds walking/driving option to setupmeeting form

--- src/components/SetUpMeeting.tsx
+++ src/components/SetUpMeeting.tsx
@@ -9,16 +9,16 @@ export const SetUpMeeting : React.FC = () => {
     const [userCoord, setUserCoord] = useState<string[]>([])
     const [friendCoord, setFriendCoord] = useState<string[]>([])
     const [isSubmitted, setIsSubmitted] = useState<boolean>(false)
+    const [transportation, setTransportation] = useState<string>('driving')
     
  
     if(isSubmitted) {
-        return <Suggestions userCoord={userCoord} friendCoord={friendCoord}/>
+        return <Suggestions userCoord={userCoord} friendCoord={friendCoord} transportation={transportation}/>
     } else {
         return <div>
         <h3>Locations:</h3>
         <MeetingMap userCoord={userCoord} friendCoord={friendCoord} />
-        <MeetingForm setUserCoord={setUserCoord} setFriendCoord={setFriendCoord} setIsSubmitted={setIsSubmitted}/>
-        
+        <MeetingForm setUserCoord={setUserCoord} setFriendCoord={setFriendCoord} setIsSubmitted={setIsSubmitted} setTransportation={setTransportation}/>
     </div>
     }
     
