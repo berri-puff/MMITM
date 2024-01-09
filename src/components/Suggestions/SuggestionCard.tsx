d19b4cb9471944d3d24b957f082c4a9e6c13f3d7
Chris Maylor <chrismaylor1@hotmail.com>
2024-01-09 15:02:27 +0000

    fixes suggestioncard bug

--- src/components/Suggestions/SuggestionCard.tsx
+++ src/components/Suggestions/SuggestionCard.tsx
@@ -30,30 +30,10 @@ export const SuggestionCard = ({
  
   const openingHours = destination.placeData.data.result.current_opening_hours.weekday_text[timeStamp.day.weekdayTextIndex]
   // console.log(openingHours, 'OPENINGHOURS')
-  return (
+  
+return (
     <>
-
-      <div>
-        <div className="card w-96 bg-base-100 shadow-xl">
-          <div className="card-body">
-            {imageUrl && <img src={imageUrl} alt="Image" />}
-            <h2 className="card-title">
-              #{index + 1} {destination.placeData.data.result.name}
-            </h2>
-            <h3>{timeStamp.day.dayName}</h3>
-            <h3>Date: {timeStamp.date}</h3>
-            <h3>Time: {timeStamp.time}</h3>
-            
-            <h3>Café</h3>
-            <h3>Rating: {destination.placeData.data.result.rating}</h3>
-            <h3>Address: {destination.address}</h3>
-            <h3>Opening hours: {openingHours}</h3>
-            <div className="travel-detail">
-              <p>Creators Journey:</p>
-              <p>From: {destination.travelDetails[0].origin}</p>
-
-      <div className="mt-10"></div>
-      <div className="card card-compact w-96 bg-base-100 shadow-2xl">
+      <div className="card card-compact w-96 bg-base-100 shadow-2xl hover:bg-secondary hover:text-secondary-content mb-10">
         <figure className="h-48 overflow-hidden">
           {imageUrl && <img src={imageUrl} alt="Image" />}
         </figure>
@@ -63,21 +43,20 @@ export const SuggestionCard = ({
             <HiOutlineStar className="inline" />
             {destination.placeData.rating}
           </h2>
+          <p>Opening Hours: {openingHours}</p>
           <h3>
             {' '}
             <HiOutlineMapPin className="inline" /> {destination.address}
           </h3>
-          <div className="divider">Itinerary</div>
+          <div className="divider">Meeting</div>
           <p>
             <HiOutlineCalendar className="inline" /> {timeStamp.date}{' '}
             <HiOutlineClock className="inline" /> {timeStamp.time}
           </p>
-
           <div className="divider">Your journey</div>
           <div className="travel-detail">
             <p>
               <HiOutlineMap className="inline" />
-
               {transportation === 'driving' ? (
                 <span> Driving </span>
               ) : (
@@ -102,7 +81,6 @@ export const SuggestionCard = ({
               {destination.travelDetails[1].origin}
             </p>
           </div>
-
           <div className="card-actions justify-end mt-5">
             <button onClick={handleClick} className="btn btn-primary mx-5">
               Confirm Meeting
@@ -112,4 +90,4 @@ export const SuggestionCard = ({
       </div>
     </>
   );
-};
+  }
