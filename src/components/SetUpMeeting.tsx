d82992b4aefc56c8e1eec04b60e6157bfa64e5d7
berri-puff <jennyzhang0207@gmail.com>
2024-01-09 13:35:18 +0000

    attempt to align items

--- src/components/SetUpMeeting.tsx
+++ src/components/SetUpMeeting.tsx
@@ -32,12 +32,12 @@ export const SetUpMeeting: React.FC = () => {
   } else {
     return (
       <section>
-        <h3 className="text-2xl font-bold text-justify mb-5">Set up a meeting here!</h3>
+        <h3 className="text-2xl font-bold justify-content-center mb-5">Set up a meeting here!</h3>
         <p className='mb-2' >To get started, fill the date, time, your's and your friend's locations and mode of transport and you're all set to find a meeting spot!</p>
         <div className="flex flex-col w-full lg:flex-row">
-        <MeetingMap className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center" userCoord={userCoord} friendCoord={friendCoord} />
-        <div className="divider lg:divider-horizontal divider-primary " ></div> 
-        <MeetingForm  className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center"
+        <MeetingMap className="grid flex-grow h-32 card bg-base-300 rounded-box" userCoord={userCoord} friendCoord={friendCoord} />
+        <div className="divider lg:divider-horizontal divider-primary" ></div> 
+        <MeetingForm  className="grid flex-grow h-32 card bg-base-300 rounded-box"
           setUserCoord={setUserCoord}
           setFriendCoord={setFriendCoord}
           setIsSubmitted={setIsSubmitted}
