7c932391bd878abd8eacf067898e1d1c808b9d1c
addleton <michaeladdleton1@gmail.com>
2024-01-10 15:10:21 +0000

    fixes merge conflicts

--- src/components/SetUpMeeting.tsx
+++ src/components/SetUpMeeting.tsx
@@ -6,25 +6,19 @@ import { Link } from "react-router-dom";
 import { UserContext } from "../contexts/UserContext";
 import { FaInfoCircle } from "react-icons/fa";
 
-
 export const SetUpMeeting: React.FC = () => {
   const [userCoord, setUserCoord] = useState<string[]>([]);
   const [friendCoord, setFriendCoord] = useState<string[]>([]);
   const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
-
   const [transportation, setTransportation] = useState<string>("walking");
-  const [timeStamp, setTimeStamp] = useState({ date: "", time: "" });
-  
-
-  const [transportation, setTransportation] = useState<string>('walking');
   const [timeStamp, setTimeStamp] = useState({
-    date: '', 
-    time: '', 
+    date: "",
+    time: "",
     day: {
-      dayName: '',
-      dayIndex: ''
-    }
-  })
+      dayName: "",
+      dayIndex: "",
+    },
+  });
 
   const { user } = useContext(UserContext);
 
@@ -51,20 +45,32 @@ export const SetUpMeeting: React.FC = () => {
   } else {
     return (
       <section className="flex flex-col items-center justify-center">
-        <h3 className="text-2xl font-bold mb-5 text-center">Set up a meeting here!</h3>
-       <FaInfoCircle/> <p className='mb-5'>To get started, fill the date, time, your location and a friend's locations, mode of transport and you're all set to find a meeting spot!</p>
+        <h3 className="text-2xl font-bold mb-5 text-center">
+          Set up a meeting here!
+        </h3>
+        <FaInfoCircle />{" "}
+        <p className="mb-5">
+          To get started, fill the date, time, your location and a friend's
+          locations, mode of transport and you're all set to find a meeting
+          spot!
+        </p>
         <div className="divider divider-success divider-secondary"></div>
         <div className="flex flex-col w-full lg:flex-row items-center justify-center">
-        <MeetingMap className="grid flex-grow h-32 card bg-base-300 rounded-box" userCoord={userCoord} friendCoord={friendCoord} />
-        <div className="divider lg:divider-horizontal divider-primary" ></div> 
-        <MeetingForm  className="grid flex-grow h-32 card bg-base-300 rounded-box"
-          setUserCoord={setUserCoord}
-          setFriendCoord={setFriendCoord}
-          setIsSubmitted={setIsSubmitted}
-          setTransportation={setTransportation}
-          setTimeStamp={setTimeStamp}
-          timeStamp={timeStamp}
-        />
+          <MeetingMap
+            className="grid flex-grow h-32 card bg-base-300 rounded-box"
+            userCoord={userCoord}
+            friendCoord={friendCoord}
+          />
+          <div className="divider lg:divider-horizontal divider-primary"></div>
+          <MeetingForm
+            className="grid flex-grow h-32 card bg-base-300 rounded-box"
+            setUserCoord={setUserCoord}
+            setFriendCoord={setFriendCoord}
+            setIsSubmitted={setIsSubmitted}
+            setTransportation={setTransportation}
+            setTimeStamp={setTimeStamp}
+            timeStamp={timeStamp}
+          />
         </div>
       </section>
     );
