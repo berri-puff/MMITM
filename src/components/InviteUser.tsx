95616981e4402529cb022a081222f05fa0d34acd
Chris Maylor <chrismaylor1@hotmail.com>
2024-01-10 12:15:53 +0000

    adds user feedback to suggestions page if there is no results, adds user feedback to invite page once invited, both changes provide navigation links, also fixed bug in areTheyOpen function, indexing was wrong

--- src/components/InviteUser.tsx
+++ src/components/InviteUser.tsx
@@ -1,9 +1,16 @@
+import { InviteConfirmation } from "./InviteUser/InviteConfirmation"
 import { InviteForm } from "./InviteUser/InviteForm"
 import { ItineraryView } from "./InviteUser/ItineraryView"
+import { useContext, useState } from "react";
 
 export const InviteUser = ({chosenMeeting, transportation, userCoord, friendCoord, timeStamp}) => {
+    const [hasClicked, setHasClicked] = useState(false)
+    const [foundUser, setFoundUser] = useState<Users[]>([]);
+    if (hasClicked === true){
+        return <InviteConfirmation chosenMeeting={chosenMeeting} timeStamp={timeStamp} foundUser={foundUser} transportation={transportation}/>
+      }
     return <div>
         <ItineraryView chosenMeeting={chosenMeeting} transportation={transportation} timeStamp={timeStamp}/>
-        <InviteForm chosenMeeting={chosenMeeting} transportation={transportation} userCoord={userCoord} friendCoord={friendCoord} timeStamp={timeStamp}/>
+        <InviteForm chosenMeeting={chosenMeeting} transportation={transportation} userCoord={userCoord} friendCoord={friendCoord} timeStamp={timeStamp} setHasClicked={setHasClicked} foundUser={foundUser} setFoundUser={setFoundUser}/>
     </div>
 }
