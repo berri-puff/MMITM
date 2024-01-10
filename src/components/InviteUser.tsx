25ddada77e466fbd59798ff15b0f6c2177eb3b22
aislingkelly <aisling.kelly45@gmail.com>
2024-01-10 10:35:33 +0000

    style the invite user card

--- src/components/InviteUser.tsx
+++ src/components/InviteUser.tsx
@@ -1,9 +1,29 @@
-import { InviteForm } from "./InviteUser/InviteForm"
-import { ItineraryView } from "./InviteUser/ItineraryView"
+import { InviteForm } from './InviteUser/InviteForm';
+import { ItineraryView } from './InviteUser/ItineraryView';
 
-export const InviteUser = ({chosenMeeting, transportation, userCoord, friendCoord, timeStamp}) => {
-    return <div>
-        <ItineraryView chosenMeeting={chosenMeeting} transportation={transportation} timeStamp={timeStamp}/>
-        <InviteForm chosenMeeting={chosenMeeting} transportation={transportation} userCoord={userCoord} friendCoord={friendCoord} timeStamp={timeStamp}/>
+export const InviteUser = ({
+  chosenMeeting,
+  transportation,
+  userCoord,
+  friendCoord,
+  timeStamp,
+}) => {
+  return (
+    <div className="container mx-auto mt-5">
+      <div className="mt-5 mb-10 w-2/3 mx-auto bg-base-300">
+        <InviteForm
+          chosenMeeting={chosenMeeting}
+          transportation={transportation}
+          userCoord={userCoord}
+          friendCoord={friendCoord}
+          timeStamp={timeStamp}
+        />
+        <ItineraryView
+          chosenMeeting={chosenMeeting}
+          transportation={transportation}
+          timeStamp={timeStamp}
+        />
+      </div>
     </div>
-}
+  );
+};
