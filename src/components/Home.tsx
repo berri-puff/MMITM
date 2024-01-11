1271bcd519a4819fba12d81bca7f4982db33e2ba
aislingkelly <aisling.kelly45@gmail.com>
2024-01-11 14:41:13 +0000

    final logo

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -10,7 +10,7 @@ export const Home = () => {
       <div className="container mx-auto mt-5">
         <div className="hero min-h-screen bg-base-200">
           <div className="hero-content flex-col lg:flex-row">
-            <img src="mm-logo.webp" className="max-w-sm" />
+            <img src="mmitm-final.png" className="max-w-sm" />
             <div>
               <h1 className="text-5xl font-bold">Meet Me in the Middle</h1>
               <p className="py-6 max-w-lg">
